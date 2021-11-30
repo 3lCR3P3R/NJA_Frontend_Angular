af9c20a59c2e83a3461e066fce57bb05d0e8a29d
3lCR3P3R <3lCR3P3R@users.noreply.github.com>
2021-11-29 22:54:00 -0500

    titulo agregado inicio

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -1,8 +1,14 @@
 import { Component } from "@angular/core";
+import { Title } from "@angular/platform-browser";
 
 @Component({
-	selector: "app-inicio",
-	templateUrl: "./inicio.component.html",
-	styleUrls: ["./inicio.component.css"],
+  selector: "app-inicio",
+  templateUrl: "./inicio.component.html",
+  styleUrls: ["./inicio.component.css"],
 })
-export class InicioComponent {}
+
+export class InicioComponent {
+  constructor(private titleService: Title) {
+    this.titleService.setTitle("N'T Just Aesthetics - NJA");
+  }
+}
